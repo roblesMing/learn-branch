1208d910338189a3b7aefbc40026bba7f67dbd49
rise_kun <tingkun_ming@163.com>
2020-08-03 00:12:22 +0800

    无

--- student_git_branch/src/App.vue
+++ student_git_branch/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">dev Hoom 555</router-link>
-      <router-link to="/about">dev About</router-link>
+      <router-link to="/">dev Hoom 33</router-link>
+      <router-link to="/about">dev About333</router-link>
     </div>
     <router-view/>
   </div>
